9022ad4be61d674d0d4af539c7ac00338b3f7ac6
Ali Kadir BAGCIOGLU <alikadirbagcioglu@gmail.com>
2020-01-20 10:48:52 +0300

    Update .hyper.js

--- .hyper.js
+++ .hyper.js
@@ -146,7 +146,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyper-search', 'hyper-material-theme', 'hyperpower'],
+  plugins: ['hyperpower', 'hyper-hypest', 'hyper-search'],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
