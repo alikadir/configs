fd75123e5e361888856ef9e9b5a0576a6ea8c958
Ali Kadir BAGCIOGLU <alikadirbagcioglu@gmail.com>
2020-04-25 18:44:30 +0300

    Create MacOS-FirstSettings.sh

---
+++ MacOS-FirstSettings.sh
@@ -0,0 +1,8 @@
+# keyboard key repeat 
+defaults write NSGlobalDomain KeyRepeat -int 0
+
+# show hidden file on finder
+defaults write com.apple.finder AppleShowAllFiles TRUE
+
+# iphone simulator full screen
+defaults write com.apple.iphonesimulator AllowFullscreenMode -bool YES
