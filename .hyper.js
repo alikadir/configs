cfea4129bb88885fe98274d69712e3674e9624f9
Ali Kadir Bagcioglu <alikadirbagcioglu@gmail.com>
2020-08-23 23:50:49 +0300

    Update .hyper.js

--- .hyper.js
+++ .hyper.js
@@ -157,7 +157,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyperpower', 'hyper-search', 'hyper-tabs-enhanced', 'hyper-statusline', 'hyper-hypest'],
+  plugins: ['hyper-search', 'hyper-tabs-enhanced', 'hyper-statusline'],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
