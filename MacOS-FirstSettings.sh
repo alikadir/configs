96bfa8a8e1405984e7b15c204c319b26a9852aa7
Ali Kadir Bagcioglu <alikadirbagcioglu@gmail.com>
2021-10-16 18:45:53 +0300

    Update MacOS-FirstSettings.sh

--- MacOS-FirstSettings.sh
+++ MacOS-FirstSettings.sh
@@ -6,3 +6,6 @@ defaults write com.apple.finder AppleShowAllFiles TRUE
 
 # iphone simulator full screen
 defaults write com.apple.iphonesimulator AllowFullscreenMode -bool YES
+
+# change git default branch name 
+git config --global init.defaultBranch main
